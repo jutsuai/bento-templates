3a1231c8b80c11f6f406729ad897d3beacb20e1e
Saidul Badhon <31236650+SaidulBadhon@users.noreply.github.com>
2024-05-23 13:35:00 +0600

    Update Badhon.jsx

--- template-1/src/components/Badhon.jsx
+++ template-1/src/components/Badhon.jsx
@@ -1,3 +1,17 @@
+import React, { useState } from "react";
+
 export default function Badhon() {
-  return <div>Badhon</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <p>Badhon</p>
+
+      <div>
+        <button onClick={() => setCounter(counter + 1)}>Increment</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      </div>
+    </div>
+  );
 }
