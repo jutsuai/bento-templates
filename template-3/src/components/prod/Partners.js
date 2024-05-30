86fa0aa201baeb1226e2d29286f44231a528bbc1
saidulbadhon <saidulbadhon@gmail.com>
2024-05-31 01:55:40 +0600

    update

--- template-3/src/components/prod/Partners.js
+++ template-3/src/components/prod/Partners.js
@@ -1,4 +1,12 @@
 export default function Partners({ props, style, id }) {
+  const logos = [
+    "https://cdn.jsdelivr.net/gh/adnansid99/theDeployer@master/Jutsu/companyLogos/spikeball-color.svg",
+    "https://cdn.jsdelivr.net/gh/adnansid99/theDeployer@master/Jutsu/companyLogos/shapeshift-color.svg",
+    "https://cdn.jsdelivr.net/gh/adnansid99/theDeployer@master/Jutsu/companyLogos/goody-color.svg",
+    "https://cdn.jsdelivr.net/gh/adnansid99/theDeployer@master/Jutsu/companyLogos/codecademy-color.svg",
+    "https://cdn.jsdelivr.net/gh/adnansid99/theDeployer@master/Jutsu/companyLogos/facepunch-color.svg",
+  ];
+
   return (
     <section
       id={id}
@@ -12,17 +20,13 @@ export default function Partners({ props, style, id }) {
         style={{ flexDirection: style?.flexDirection }}
         className="py-20 pb-28 mx-auto px-4 flex flex-col items-center justify-center gap-20 container"
       >
-        {/* <h2 className="text-3xl md:text-4xl font-semibold dark:text-white text-black text-center">
-          {props?.headerText}
-        </h2> */}
         <div className="grid grid-cols-2 gap-10 sm:grid-cols-[repeat(auto-fit,_minmax(140px,1fr))] place-items-center xl:px-0 px-8 items-center w-full">
-          {props?.logos?.map((logo, index) => (
+          {(logos || props?.logos)?.map((logo, index) => (
             <img
               key={index}
               src={logo}
               alt="logos"
               className="backface-hidden grayscale pointer-events-none transform-gpu select-none !rounded-none dark:brightness-[0.35] dark:grayscale dark:invert dark:saturate-[0.15] dark:sepia-[0.01]"
-              // className="aspect-video grayscale rounded-lg object-cover object-center"
             />
           ))}
         </div>
