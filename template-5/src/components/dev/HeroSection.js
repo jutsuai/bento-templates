9058f35dbcc5c17af698ade45d01a2df7eb8229d
Adnan Siddiqui <asr3012003@gmail.com>
2024-06-09 22:35:15 +0600

    init

--- template-5/src/components/dev/HeroSection.js
+++ template-5/src/components/dev/HeroSection.js
@@ -17,7 +17,8 @@ export default function HeroSection({ props, style, id }) {
       `${style?.paddingTop}px ${style?.paddingRight}px ${style?.paddingBottom}px ${style?.paddingLeft}px`};
     margin: ${({ style }) =>
       `${style?.marginTop}px ${style?.marginRight}px ${style?.marginBottom}px ${style?.marginLeft}px`};
-    background-image: ${({ props }) => `url(${props?.backgroundImageUrl})`};
+    background-image: ${({ props }) =>
+      `url(https://images.unsplash.com/photo-1524802414218-3983cd08ed53?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)`};
     background-size: cover;
     background-position: center;
   `;
--- template-5/src/components/prod/Article.js
+++ template-5/src/components/prod/Article.js
@@ -1,11 +1,5 @@
 import styled from "styled-components";
 
-// const bp = {
-//   sm: "36rem",
-//   md: "42rem",
-//   lg: "48rem",
-//   xl: "56rem",
-// };
 const bp = {
   sm: "640px",
   md: "768px",
