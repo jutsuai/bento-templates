2ad3293a72fb64bec41ec623bcbc5e64527ffc56
Adnan Siddiqui <asr3012003@gmail.com>
2024-06-16 13:30:37 +0600

    update

--- template-5/src/components/prod/PrimaryCTA.js
+++ template-5/src/components/prod/PrimaryCTA.js
@@ -95,13 +95,17 @@ export default function PrimaryCTA({ props, style, id }) {
               backgroundColor: "rgb(31 41 55)",
             }}
           >
-            <i
-              className="fa-solid fa-arrow-right"
+            <svg
               style={{
-                fontSize: "0.75rem",
-                lineHeight: "1rem",
+                width: "0.7rem",
+                transform: "rotate(-0.25turn)",
+                fill: "black",
               }}
-            ></i>
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+            </svg>
           </div>
         </button>
       </PrimaryCTAContent>
--- template-5/src/components/prod/SecondaryCTA.js
+++ template-5/src/components/prod/SecondaryCTA.js
@@ -78,14 +78,9 @@ export default function SecondaryCTA({ props, style, id }) {
       <SecondaryCTAContent>
         <SecondaryCTADesign>
           {[...new Array(100)].map((_, index) => (
-            <i
-              class="fa-solid fa-plus"
-              style={{
-                fontSize: "0.75rem",
-                opacity: "0.6",
-              }}
-              key={index}
-            />
+            <div key={index} style={{ fontSize: "0.75rem" }}>
+              +
+            </div>
           ))}
         </SecondaryCTADesign>
         <SecondaryCTAData>
@@ -128,13 +123,17 @@ export default function SecondaryCTA({ props, style, id }) {
                   color: "#28282c",
                 }}
               >
-                <i
-                  className="fa-solid fa-arrow-right"
+                <svg
                   style={{
-                    fontSize: "0.75rem",
-                    lineHeight: "1rem",
+                    width: "0.7rem",
+                    transform: "rotate(-0.25turn)",
+                    fill: "black",
                   }}
-                ></i>
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 384 512"
+                >
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
               </div>
             </button>
           </div>
